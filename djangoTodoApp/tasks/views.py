cda83754a77f5d23de8dadbfadcbe046733ca4d0
truonghoangthuan <tthun32@gmail.com>
2021-02-12 22:47:25 +0700

    12/02/2021 22h47

--- djangoTodoApp/tasks/views.py
+++ djangoTodoApp/tasks/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello')
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+
+    return render(request, 'tasks/list.html', context)
