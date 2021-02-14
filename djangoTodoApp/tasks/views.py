720827117f4d070a9e75842fa315bc121ca19c0f
truonghoangthuan <tthun32@gmail.com>
2021-02-14 23:13:46 +0700

    14/02/2021 23h13

--- djangoTodoApp/tasks/views.py
+++ djangoTodoApp/tasks/views.py
@@ -32,3 +32,14 @@ def updateTask(request, pk):
     context = {'form': form}
 
     return render(request, 'tasks/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'tasks/delete_task.html', context)
